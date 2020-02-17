331d67d25205328affb6288d19162346e2a7ec20
skyling <shilibing012944@hfjy.com>
2020-02-17 18:20:07 +0800

    chore(release): 1.26.2

--- dist/heyui.esm.js
+++ dist/heyui.esm.js
@@ -1,7 +1,7 @@
 /**
   * sky-heyui - UI Toolkit for Web, Vue2.0.
   *
-  * @version v1.26.1
+  * @version v1.26.2
   * @homepage http://www.heyui.top
   * @copyright Copyright © 2017-present Lan
   * @license MIT
